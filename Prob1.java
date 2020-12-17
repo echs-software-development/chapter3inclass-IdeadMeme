4b3041ed88e915c25eb4ff0fa4e5160884f6c0f0
ECHS Marist College/Newburgh Free Academy <ethomascappello@necsd.net>
2020-12-17 20:01:27 +0000

    chang 2

--- Prob1.java
+++ Prob1.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 
 public class Prob1{
   public static void main(String[] args){
-    
+
+ Scanner scan = new scanner(System.in);
+ System.out.println("Enter 2 integers: ");
+
+ int num1 = scan.nextInt();
+ int num2 = scan.nextInt();
+
+ if(num1 == num2)
+ System.out.println("YES");
+
+ 
 
 
     
--- Prob2.java
+++ Prob2.java
@@ -2,9 +2,21 @@ import java.util.Scanner;
 
 public class Prob2  {
   public static void main(String[] args){
-    
+   Scanner scan = new Scanner(System.in);
+   system.out.println("Enter4 decimals: ");
+
+   double num1 = scan.nextDouble();
+   double num2 = scan.nextDouble();
+   double num3 = scan.nextDouble();
+   double num4 = scan.nextDouble();
+
+   double all = (num1 + num2 + num3 + num4);
+
+   if (all >=89.5) /4 ){
+   System.out.println("Above Average ");
 
 
     
+    }
   }
 }
