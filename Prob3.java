b98edc28bb69b1a86f9369fa725dcfeec59ac74c
Matthew Jabbour <mj1769@students.necsd.net>
2020-12-17 20:39:58 +0000

    3 and 4

--- Prob3.java
+++ Prob3.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 public class Prob3  {
   public static void main(String[] args){
-    
+
+  Scanner scan = new Scanner(System.in);
+  System.out.println("What is Your Grade 1 through 4");
+
+ int num1 = scan.nextInt();
+
+  if(num1 > 2)
+  System.out.println("A");
+ else
+  if(num1 < 2)
+  system.out.println("F"); 
 
 
-    
+
+
   }
 }
--- Prob4.java
+++ Prob4.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 
 public class Prob4  {
   public static void main(String[] args){
-    
+
+ Scanner scan = new scanner(System.in);
+ System.out.println("Enter temperture ");
+
+ int num1 = scan.nextInt();
+
+ if (num1 < 100)
+ system.out.println("Warning");
+ else
+ if (num1 > 100)
+ System.out.println("ok");
+ 
+   
 
 
     
